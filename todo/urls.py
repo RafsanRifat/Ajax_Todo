3bb05681ee42a9d6a01c7e4f950aa9733dfcea2b
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-17 18:43:22 +0600

    working on ajax

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, todo_list
 
 urlpatterns = [
     path('', home, name='home'),
+    path('todo_list/', todo_list, name='todo_list'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
+from django.http import JsonResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return JsonResponse(todos)
