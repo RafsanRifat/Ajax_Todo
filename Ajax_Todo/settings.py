b8b62ab570794b7d15d1b639f668f8eb11199f22
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-17 15:37:32 +0600

    todo app created

--- Ajax_Todo/settings.py
+++ Ajax_Todo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Ajax_Todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
