0bdf7031f7fba064ef906a7a53737edb503a56ab
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-20 13:24:09 +0600

    new todo creating

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, todo_list
+from .views import home, todo_list, create_todo
 
 urlpatterns = [
     path('', home, name='home'),
     path('todo_list/', todo_list, name='todo_list'),
+    path('create_todo/', create_todo, name='create_todo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -11,3 +11,10 @@ def home(request):
 def todo_list(request):
     todos = Todo.objects.all()
     return JsonResponse({'todos': list(todos.values())})
+
+def create_todo(request):
+    if request.method == 'POST':
+        todo_name = request.POST.get('todo_name')
+        todo = Todo.objects.create(task=todo_name)
+        print(todo_name)
+        return JsonResponse({'todo_name': todo_name})
