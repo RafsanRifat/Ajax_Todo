90cfec18961fa1f984d9699f0c108045b42dc788
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-20 13:37:49 +0600

    todo create complete

--- todo/views.py
+++ todo/views.py
@@ -16,5 +16,4 @@ def create_todo(request):
     if request.method == 'POST':
         todo_name = request.POST.get('todo_name')
         todo = Todo.objects.create(task=todo_name)
-        print(todo_name)
-        return JsonResponse({'todo_name': todo_name})
+        return JsonResponse({'todo_name': todo.task})
