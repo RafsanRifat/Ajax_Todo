a6204bb993977c07938456541de9443db2a9ca72
RafsanRifat <rafsan.rifat255@gmail.com>
2022-02-21 13:17:57 +0600

    getting data from database when editing

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, todo_list, create_todo, delete
+from .views import home, todo_list, create_todo, delete, edit
 
 urlpatterns = [
     path('', home, name='home'),
     path('todo_list/', todo_list, name='todo_list'),
     path('create_todo/', create_todo, name='create_todo'),
     path('delete/', delete, name='delete'),
+    path('edit/', edit, name='edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -12,6 +12,7 @@ def todo_list(request):
     todos = Todo.objects.all()
     return JsonResponse({'todos': list(todos.values())})
 
+
 def create_todo(request):
     if request.method == 'POST':
         todo_name = request.POST.get('todo_name')
@@ -20,10 +21,20 @@ def create_todo(request):
         print(todo_id)
         return JsonResponse({'todo_name': todo.task, 'todo_id': todo_id})
 
+
 def delete(request):
     if request.method == "POST":
         id = request.POST.get('sid')
         print(id)
         item = Todo.objects.get(pk=id)
         item.delete()
-        return JsonResponse({'success': 'Successfully deleted'})
+        return JsonResponse({'status': 1})
+
+
+def edit(request):
+    if request.method == 'POST':
+        id = request.POST.get('sid')
+        update_item = Todo.objects.get(pk=id)
+        selected_item = update_item.task
+        print(selected_item)
+        return JsonResponse({'selected_item': update_item.task})
